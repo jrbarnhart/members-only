67b5dea992a2549ac9d2be5f439eafda1411e697
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-18 14:32:51 -0500

    Add placeholder routes for sign up form

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/sign-up", (req, res, next) => {
+  res.send("user sign up form");
+});
+router.post("/sign-up", (req, res, next) => {
+  res.send("user sign up form post");
 });
 
 module.exports = router;
