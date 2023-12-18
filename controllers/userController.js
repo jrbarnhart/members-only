332e20de14b364888fb10586720355c55b6ef9cf
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-18 16:24:02 -0500

    Add user controller file

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+// User sign up get
+
+// User sign up post
+
+// User log in get
