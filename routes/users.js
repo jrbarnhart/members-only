60304b947f5b12254035a94b9603e879669ccf6f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-18 16:15:19 -0500

    Add placeholder route for log in

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,8 @@ router.post("/sign-up", (req, res, next) => {
   res.send("user sign up form post");
 });
 
+router.get("/log-in", (req, res, next) => {
+  res.send("user login");
+});
+
 module.exports = router;
