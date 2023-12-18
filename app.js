e01364ad2dc34d342fdb1bf4b203eb675807da89
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-18 16:47:56 -0500

    Add comments

--- app.js
+++ app.js
@@ -24,12 +24,14 @@ main().catch((err) => console.log(err));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Default express generator middleware
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routers
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
