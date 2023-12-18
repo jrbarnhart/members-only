8d321b63e00d4cdbb907a42bd2750f949041bf21
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-18 16:32:20 -0500

    Import and use the user controller

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const userController = require("../controllers/userController");
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-router.get("/sign-up", (req, res, next) => {
-  res.send("user sign up form");
-});
-router.post("/sign-up", (req, res, next) => {
-  res.send("user sign up form post");
-});
+// Sign up
+router.get("/sign-up", userController.signup_get);
+router.post("/sign-up", userController.signup_post);
 
-router.get("/log-in", (req, res, next) => {
-  res.send("user login");
-});
+// Log in
+router.get("/log-in", userController.login_get);
+router.post("/log-in", userController.login_post);
 
 module.exports = router;
