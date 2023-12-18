8fc291d62e625897bfb9b23ffa92f6e1e6f64fda
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-18 16:27:03 -0500

    Add placeholder handlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,21 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 // User sign up get
+exports.signup_get = asyncHandler(async (req, res, next) => {
+  res.send("Sign up form");
+});
 
 // User sign up post
+exports.signup_post = asyncHandler(async (req, res, next) => {
+  res.send("Sign up post");
+});
 
 // User log in get
+exports.login_get = asyncHandler(async (req, res, next) => {
+  res.send("Login form");
+});
+
+// User log in post
+exports.login_post = asyncHandler(async (req, res, next) => {
+  res.send("Login post");
+});
