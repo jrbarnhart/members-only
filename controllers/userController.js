dbd4abfea7bd7f44e14512cfe208f0aaa80b3aee
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-01-15 17:15:20 -0500

    Sanitize and validate first/last name and username

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,9 +7,33 @@ exports.signup_get = asyncHandler(async (req, res, next) => {
 });
 
 // User sign up post
-exports.signup_post = asyncHandler(async (req, res, next) => {
-  res.send("Sign up post");
-});
+exports.signup_post = [
+  body("name-given")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("First name required")
+    .isLength({ min: 1, max: 200 })
+    .withMessage("First name must be 1-200 characters")
+    .escape(),
+  body("name-family")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("Last name required")
+    .isLength({ min: 1, max: 200 })
+    .withMessage("Last name must be 1-200 characters")
+    .escape(),
+  body("username")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("Username required")
+    .isLength({ min: 3, max: 200 })
+    .withMessage("Username must be 3-200 characters")
+    .isEmail()
+    .withMessage("Username must be an email")
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {}),
+];
 
 // User log in get
 exports.login_get = asyncHandler(async (req, res, next) => {
