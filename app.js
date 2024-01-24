a4262fd18351b50017a12c1c3d757000389ea5db
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-01-24 16:53:52 -0500

    Disable failure flash

--- app.js
+++ app.js
@@ -31,24 +31,21 @@ app.set("view engine", "pug");
 
 // Passport setup
 passport.use(
-  new LocalStrategy(
-    { failureFlash: true },
-    async (username, password, done) => {
-      try {
-        const user = await User.findOne({ username: username });
-        if (!user) {
-          return done(null, false, { message: "Incorrect username" });
-        }
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-          return done(null, false, { message: "Incorrect password" });
-        }
-        return done(null, user);
-      } catch (err) {
-        return done(err);
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
       }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
     }
-  )
+  })
 );
 
 passport.serializeUser((user, done) => {
