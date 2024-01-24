d4f5b154583d5fc4fa7ee56b9b9ed750737a55f2
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-01-24 16:30:10 -0500

    Add file for messageController

---
+++ controllers/messageController.js
@@ -0,0 +1,8 @@
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+// Create message get
+
+// Create message post
+
+// Delete message post
