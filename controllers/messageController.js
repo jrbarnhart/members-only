b56b8d7f86948c709ff849da15bff71142b7e1c3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-01-27 15:11:51 -0500

    Add create message get handler

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,7 +2,9 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 // Create message get
-
+exports.create_message_get = (req, res, next) => {
+  res.render("create_message", { title: "Create Message" });
+};
 // Create message post
 
 // Delete message post
