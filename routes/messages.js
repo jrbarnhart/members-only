a124ac9578844fd81ed199596e5db722c47ee248
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-01-27 16:42:11 -0500

    Redirect to index for default route

--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const messageController = require("../controllers/messageController");
 
+//Default route
+router.get("/", (req, res, next) => {
+  res.redirect("/");
+});
+
 // Create message
 router.get("/create-message", messageController.create_message_get);
 router.post("/create-message", messageController.create_message_post);
