b2e0bcf487b2d43cfb31c79ea3f2d366e8e08727
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-01-28 15:57:50 -0500

    Get messages and filter based on currentUser

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
+
+const Message = require("../models/message");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Message Board" });
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    let messages;
+    if (res.locals.currentUser) {
+      messages = await Message.find()
+        .sort({ timestamp: -1 })
+        .limit(10)
+        .populate("author")
+        .exec();
+    } else {
+      messages = await Message.find()
+        .sort({ timestamp: -1 })
+        .select("-author -timestamp")
+        .limit(10)
+        .exec();
+    }
+    res.render("index", { title: "Message Board", messages: messages });
+  })
+);
 
 module.exports = router;
