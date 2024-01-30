30ccbfe8fbd8f7794baf57eecf55ace324f7154e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-01-30 18:13:21 -0500

    Use correct route handler

--- routes/messages.js
+++ routes/messages.js
@@ -13,10 +13,10 @@ router.get("/create-message", messageController.create_message_get);
 router.post("/create-message", messageController.create_message_post);
 
 // Delete message
-router.get("/delete-message/:id", messageController.delete_message_get);
 router.post(
   "/delete-message/:id/confirm",
   messageController.delete_message_post
 );
+router.get("/delete-message/:id", messageController.delete_message_get);
 
 module.exports = router;
