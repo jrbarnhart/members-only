f30d191b11d71013e74d959ad3df5de23e00e996
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-01-30 18:01:19 -0500

    Add route for delete message post

--- routes/messages.js
+++ routes/messages.js
@@ -13,6 +13,10 @@ router.get("/create-message", messageController.create_message_get);
 router.post("/create-message", messageController.create_message_post);
 
 // Delete message
-router.post("/delete-message", messageController.delete_message_post);
+router.get("/delete-message/:id", messageController.delete_message_get);
+router.post(
+  "/delete-message/:id/confirm",
+  messageController.delete_message_post
+);
 
 module.exports = router;
